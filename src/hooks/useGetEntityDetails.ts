73ec9e4df699c9ca68faa360ede420a2bb0e33b4
jakerdxworks <jakephucmai@gmail.com>
2024-11-11 17:50:08 -0500

    asda

--- src/hooks/useGetEntityDetails.ts
+++ src/hooks/useGetEntityDetails.ts
@@ -1,10 +1,21 @@
 import { useGateway } from './useGateway';
+import { StateEntityDetailsOperationRequest } from '@radixdlt/babylon-gateway-api-sdk';
 
-export const useGetEntityDetails = (address: string) => {
+export const useGetEntityDetails = async (address: string) => {
   const gatewayApi = useGateway();
 
-  gatewayApi.state.getEntityMetadata(address).then((res) =>
+  const request: StateEntityDetailsOperationRequest = {
+    stateEntityDetailsRequest: {
+        addresses: [address], // Replace with your actual addresses
+        at_ledger_state: null, // Optional
+        opt_ins: {}, // Optional, replace with actual opt-in options if needed
+        aggregation_level: undefined, // Optional
+    },
+};
   
-  
-  )
+  const response = await gatewayApi.state.innerClient.stateEntityDetails(request);
+  console.log(response);
+
+
+  // )
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { RdtProvider } from './contexts/RdtProvider.tsx';
+import { GatewayApiClient } from '@radixdlt/babylon-gateway-api-sdk'
+import { GatewayProvider } from './contexts/GatewayProvider.tsx'
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -12,30 +14,24 @@ const rdt = RadixDappToolkit({
   dAppDefinitionAddress:
     'account_tdx_2_12y7yjk7h0k5cd45au6kthphe83pxrutencm9xh3vtu3p8kllcgxf33',
   networkId: RadixNetwork.Stokenet, // network ID 2 is for the stokenet test network, network ID 1 is for mainnet
-  applicationName: 'Yield AMM dApp',
+  applicationName: 'Wild dApp',
   applicationVersion: '1.0.0',
 });
 console.log('dApp Toolkit: ', rdt);
 
+const gateway = GatewayApiClient.initialize({
+  basePath: 'https://stokenet-gateway.radixdlt.com',
+  applicationName: 'Wild dApp',
+  applicationDappDefinitionAddress: 'account_tdx_2_12y7yjk7h0k5cd45au6kthphe83pxrutencm9xh3vtu3p8kllcgxf33',
+})
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GatewayProvider value={gateway}>
     <RdtProvider value={rdt}>
       <App />
     </RdtProvider>
+    </GatewayProvider>
   </React.StrictMode>
 );
 
-// Ensure the DApp Toolkit is loaded before rendering
-// const waitForToolkit = () => {
-//   if (window.RadixDappToolkit) {
-//     root.render(
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>
-//     );
-//   } else {
-//     setTimeout(waitForToolkit, 50);
-//   }
-// };
-
-// waitForToolkit();
